22d5825c9026e2a0bb34fb7fb307fcf2ddf37842
Lyrut <txuj91@gmail.com>
2020-11-10 22:08:57 +0100

    Updates

---
+++ precache-manifest.459d274d91beddd3b6051e94b833e927.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bdd92f00f17b54105a2349634e04a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5bf428a08fa0a53024",
+    "url": "/static/css/main.ed5e641b.chunk.css"
+  },
+  {
+    "revision": "3c84a43ce885804af8da",
+    "url": "/static/js/2.e5d36a05.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.e5d36a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5bf428a08fa0a53024",
+    "url": "/static/js/main.ba5ee18a.chunk.js"
+  },
+  {
+    "revision": "054c07f8bd486207b466",
+    "url": "/static/js/runtime-main.3ca9883d.js"
+  },
+  {
+    "revision": "e1475fa9d0d3433318bc200cc45e002e",
+    "url": "/static/media/FuturaBook-Regular.e1475fa9.otf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
